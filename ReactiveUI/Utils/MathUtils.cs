c0c6fadc22ce7f4ff074c06187805a7dc140a7f2
hermanest <nicedevelooper@gmail.com>
2024-10-10 23:01:15 +0300

    Added 'RoundStepped' function to 'MathUtils'.

--- ReactiveUI/Utils/MathUtils.cs
+++ ReactiveUI/Utils/MathUtils.cs
@@ -1,4 +1,5 @@
 ﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Reactive;
 
@@ -6,5 +7,11 @@ namespace Reactive;
 public static class MathUtils {
     public static float Map(float val, float inMin, float inMax, float outMin, float outMax) {
         return (val - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+    
+    public static float RoundStepped(float value, float step, float startValue = 0) {
+        if (step is 0) return value;
+        var relativeValue = value - startValue;
+        return startValue + Mathf.Round(relativeValue / step) * step;
     }
 }
