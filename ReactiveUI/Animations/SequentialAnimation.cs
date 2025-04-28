4668c7495b988bf2f0bcc63ae5650a76cf3f3f38
hermanest <nicedevelooper@gmail.com>
2025-04-28 23:43:18 +0300

    Fixed SequentialAnimation playing even when animation is not started.

--- ReactiveUI/Animations/SequentialAnimation.cs
+++ ReactiveUI/Animations/SequentialAnimation.cs
@@ -21,14 +21,21 @@ public class SequentialAnimation : ISharedAnimation {
     private readonly IEnumerable<ISharedAnimation> _animations;
     private readonly Action _onStart;
     private bool _isFinished;
+    private bool _isPlaying;
 
     public void FinishToEnd() {
+        _isPlaying = false;
+        _isFinished = true;
+        
         foreach (var animation in _animations) {
             animation.FinishToEnd();
         }
     }
 
     public void Finish() {
+        _isPlaying = false;
+        _isFinished = true;
+        
         foreach (var animation in _animations) {
             animation.Finish();
         }
@@ -36,10 +43,15 @@ public class SequentialAnimation : ISharedAnimation {
 
     public void Play() {
         _isFinished = false;
+        _isPlaying = true;
         _onStart();
     }
 
     public void OnUpdate() {
+        if (!_isPlaying) {
+            return;
+        }
+        
         _isFinished = true;
 
         foreach (var animation in _animations) {
