19013f9b2be565b759165d0a69fcde88dfa01077
hermanest <nicedevelooper@gmail.com>
2025-05-11 14:47:36 +0300

    Added documentation to LayoutExtensions.

--- ReactiveUI/Layout/LayoutExtensions.cs
+++ ReactiveUI/Layout/LayoutExtensions.cs
@@ -9,6 +9,25 @@ namespace Reactive {
     public static class LayoutExtensions {
         #region Flex Item
 
+        /// <summary>
+        /// Configures a component as a flex item with the specified layout properties.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="modifier">A <see cref="YogaModifier"/> instance.</param>
+        /// <param name="flex">A flex value.</param>
+        /// <param name="flexGrow">A flex grow value.</param>
+        /// <param name="flexShrink">A flex shrink value.</param>
+        /// <param name="basis">A flex basis value.</param>
+        /// <param name="size">A size of the item.</param>
+        /// <param name="minSize">A minimum size of the item.</param>
+        /// <param name="maxSize">A maximum size of the item.</param>
+        /// <param name="margin">A margin of the item.</param>
+        /// <param name="aspectRatio">An aspect ratio of the item.</param>
+        /// <param name="position">A position of the item.</param>
+        /// <param name="positionType">A position type of the item.</param>
+        /// <param name="alignSelf">An alignment of the item.</param>
+        /// <returns>The configured component.</returns>
         public static T AsFlexItem<T>(
             this T component,
             out YogaModifier modifier,
@@ -45,6 +64,24 @@ namespace Reactive {
             );
         }
 
+        /// <summary>
+        /// Configures a component as a flex item with the specified layout properties.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="flex">A flex value.</param>
+        /// <param name="flexGrow">A flex grow value.</param>
+        /// <param name="flexShrink">A flex shrink value.</param>
+        /// <param name="basis">A flex basis value.</param>
+        /// <param name="size">A size of the item.</param>
+        /// <param name="minSize">A minimum size of the item.</param>
+        /// <param name="maxSize">A maximum size of the item.</param>
+        /// <param name="margin">A margin of the item.</param>
+        /// <param name="aspectRatio">An aspect ratio of the item.</param>
+        /// <param name="position">A position of the item.</param>
+        /// <param name="positionType">A position type of the item.</param>
+        /// <param name="alignSelf">An alignment of the item.</param>
+        /// <returns>The configured component.</returns>
         public static T AsFlexItem<T>(
             this T component,
             float? flex = null,
@@ -78,6 +115,27 @@ namespace Reactive {
             );
         }
 
+        /// <summary>
+        /// Configures a component as a flex item with the specified layout properties using an already created modifier.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="layoutModifier">A layout modifier to use, if any.</param>
+        /// <param name="setModifierCallback">A callback to assign the modifier to the component.</param>
+        /// <param name="yogaModifier">A YogaModifier used for configuration.</param>
+        /// <param name="flex">A flex value.</param>
+        /// <param name="flexGrow">A flex grow value.</param>
+        /// <param name="flexShrink">A flex shrink value.</param>
+        /// <param name="basis">A flex basis value.</param>
+        /// <param name="size">A size of the item.</param>
+        /// <param name="minSize">A minimum size of the item.</param>
+        /// <param name="maxSize">A maximum size of the item.</param>
+        /// <param name="margin">A margin of the item.</param>
+        /// <param name="aspectRatio">An aspect ratio of the item.</param>
+        /// <param name="position">A position of the item.</param>
+        /// <param name="positionType">A position type of the item.</param>
+        /// <param name="alignSelf">An alignment of the item.</param>
+        /// <returns>The configured component.</returns>
         public static T AsFlexItem<T>(
             this T component,
             ILayoutModifier? layoutModifier,
@@ -155,6 +213,22 @@ namespace Reactive {
 
         #region Flex Group
 
+        /// <summary>
+        /// Configures a component as a flex group (container) with the specified layout properties.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="direction">A flex direction.</param>
+        /// <param name="justifyContent">A justification of content.</param>
+        /// <param name="alignItems">An alignment of items.</param>
+        /// <param name="alignContent">An alignment of content.</param>
+        /// <param name="wrap">A wrap mode.</param>
+        /// <param name="overflow">An overflow behavior.</param>
+        /// <param name="padding">A padding of the group.</param>
+        /// <param name="gap">A gap between items.</param>
+        /// <param name="constrainHorizontal">Whether to constrain horizontally.</param>
+        /// <param name="constrainVertical">Whether to constrain vertically.</param>
+        /// <returns>The configured component.</returns>
         public static T AsFlexGroup<T>(
             this T component,
             FlexDirection? direction = null,
@@ -184,6 +258,23 @@ namespace Reactive {
             );
         }
 
+        /// <summary>
+        /// Configures a component as a flex group (container) with the specified layout properties.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="layoutController">A <see cref="YogaLayoutController"/> instance.</param>
+        /// <param name="direction">A flex direction.</param>
+        /// <param name="justifyContent">A justification of content.</param>
+        /// <param name="alignItems">An alignment of items.</param>
+        /// <param name="alignContent">An alignment of content.</param>
+        /// <param name="wrap">A wrap mode.</param>
+        /// <param name="overflow">An overflow behavior.</param>
+        /// <param name="padding">A padding of the group.</param>
+        /// <param name="gap">A gap between items.</param>
+        /// <param name="constrainHorizontal">Whether to constrain horizontally.</param>
+        /// <param name="constrainVertical">Whether to constrain vertically.</param>
+        /// <returns>The configured component.</returns>
         public static T AsFlexGroup<T>(
             this T component,
             out YogaLayoutController layoutController,
@@ -247,6 +338,14 @@ namespace Reactive {
 
         #region Rect
 
+        /// <summary>
+        /// Sets the size delta of the component's RectTransform.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="width">A width to set.</param>
+        /// <param name="height">A height to set.</param>
+        /// <returns>The configured component.</returns>
         public static T WithSizeDelta<T>(
             this T component,
             float width,
@@ -255,6 +354,16 @@ namespace Reactive {
             return component.AsRectItem(new(width, height));
         }
 
+        /// <summary>
+        /// Configures the component's RectTransform with the specified properties.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to configure.</param>
+        /// <param name="sizeDelta">A size delta to set.</param>
+        /// <param name="anchorMin">A minimum anchor to set.</param>
+        /// <param name="anchorMax">A maximum anchor to set.</param>
+        /// <param name="pivot">A pivot to set.</param>
+        /// <returns>The configured component.</returns>
         public static T AsRectItem<T>(
             this T component,
             Vector2? sizeDelta = null,
@@ -270,17 +379,33 @@ namespace Reactive {
             return component;
         }
 
+        /// <summary>
+        /// Expands the RectTransform of a component to fill its parent.
+        /// </summary>
+        /// <typeparam name="T">A component type.</typeparam>
+        /// <param name="component">A component to expand.</param>
+        /// <returns>The expanded component.</returns>
         public static T WithRectExpand<T>(this T component) where T : IReactiveComponent {
             component.ContentTransform.WithRectExpand();
             return component;
         }
 
+        /// <summary>
+        /// Expands the RectTransform of a layout item to fill its parent.
+        /// </summary>
+        /// <param name="component">A layout item to expand.</param>
+        /// <returns>The expanded layout item.</returns>
         public static ILayoutItem WithRectExpand(this ILayoutItem component) {
             component.BeginApply().WithRectExpand();
             component.EndApply();
             return component;
         }
 
+        /// <summary>
+        /// Expands the RectTransform to fill its parent.
+        /// </summary>
+        /// <param name="component">A RectTransform to expand.</param>
+        /// <returns>The expanded RectTransform.</returns>
         public static RectTransform WithRectExpand(this RectTransform component) {
             component.anchorMin = Vector2.zero;
             component.anchorMax = Vector2.one;
@@ -291,6 +416,13 @@ namespace Reactive {
 
         #endregion
 
+        /// <summary>
+        /// Copies properties from another layout modifier.
+        /// </summary>
+        /// <typeparam name="T">A layout modifier type.</typeparam>
+        /// <param name="modifier">A modifier to copy to.</param>
+        /// <param name="apModifier">A modifier to copy from.</param>
+        /// <returns>The updated modifier.</returns>
         public static T With<T>(this T modifier, ILayoutModifier apModifier) where T : ILayoutModifier {
             modifier.CopyFrom(apModifier);
             return modifier;
