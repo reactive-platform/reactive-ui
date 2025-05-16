38bf4a50428bd6d744d2a1ba5775d23a260c1ae7
Hermanest <mutcmk@gmail.com>
2025-05-16 21:45:49 +0300

    Some docs for IReactiveModuleBinder and IReactiveModule.

--- ReactiveUI/Patterns/Modules/IReactiveModule.cs
+++ ReactiveUI/Patterns/Modules/IReactiveModule.cs
@@ -1,10 +1,24 @@
 ﻿using JetBrains.Annotations;
 
 namespace Reactive {
+    /// <summary>
+    /// Represents a module object that can be registered into the component via <see cref="IReactiveModuleBinder"/> interface.
+    /// </summary>
     [PublicAPI]
     public interface IReactiveModule {
+        /// <summary>
+        /// Called on each Update cycle if a component is enabled.
+        /// </summary>
         void OnUpdate();
+        
+        /// <summary>
+        /// Called after binding to a component.
+        /// </summary>
         void OnBind();
+        
+        /// <summary>
+        /// Called after unbinding from a component.
+        /// </summary>
         void OnUnbind();
     }
 }
--- ReactiveUI/Patterns/Modules/IReactiveModuleBinder.cs
+++ ReactiveUI/Patterns/Modules/IReactiveModuleBinder.cs
@@ -1,9 +1,21 @@
 ﻿using JetBrains.Annotations;
 
 namespace Reactive {
+    /// <summary>
+    /// Represents a component alignable for module binding. Available for any component that derives <see cref="ReactiveComponent"/>.
+    /// </summary>
     [PublicAPI]
     public interface IReactiveModuleBinder {
+        /// <summary>
+        /// Binds a specified module.
+        /// </summary>
+        /// <param name="module">A module to bind.</param>
         void BindModule(IReactiveModule module);
+        
+        /// <summary>
+        /// Unbinds a specified module.
+        /// </summary>
+        /// <param name="module">A module to unbind.</param>
         void UnbindModule(IReactiveModule module);
     }
 }
