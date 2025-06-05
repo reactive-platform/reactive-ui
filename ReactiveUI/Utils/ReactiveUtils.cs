b992160d7955f1e5b3e7efef43723897c622aed4
Hermanest <mutcmk@gmail.com>
2025-06-05 17:24:38 +0300

    Added methods to create a sprite from RenderTexture to ReactiveUtils.

--- ReactiveUI/Utils/ReactiveUtils.cs
+++ ReactiveUI/Utils/ReactiveUtils.cs
@@ -7,6 +7,19 @@ namespace Reactive {
     public static class ReactiveUtils {
         #region Sprites
 
+        public static Texture2D CreateTexture(RenderTexture rt) {
+            RenderTexture active = RenderTexture.active;
+            RenderTexture.active = rt;
+            
+            var texture2D = new Texture2D(rt.width, rt.height);
+            texture2D.wrapMode = rt.wrapMode;
+            texture2D.ReadPixels(new Rect(0.0f, 0.0f, (float) texture2D.width, (float) texture2D.height), 0, 0);
+            texture2D.Apply();
+            
+            RenderTexture.active = active;
+            return texture2D;
+        }
+        
         public static Texture2D? CreateTexture(byte[] bytes) {
             if (bytes.Length == 0) return null;
             var texture = new Texture2D(1, 1, TextureFormat.RGBA32, false, false);
@@ -23,6 +36,12 @@ namespace Reactive {
                 new Rect(0f, 0f, texture.width, texture.height),
                 new Vector2(texture.width / 2f, texture.height / 2f)
             );
+        }
+        
+        public static Sprite? CreateSprite(RenderTexture rendTexture) {
+            var texture = CreateTexture(rendTexture);
+
+            return CreateSprite(texture);
         }
 
         #endregion
